2f1b783428df4a3c239f01d3c96f99094c388bcc
johnhuh619 <jaewonheo666@gmail.com>
2019-12-06 14:48:32 +0900

    id & password func

--- project.c
+++ project.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+//bank account program
+char ID_pass[2][9];//ID&password global
+int account = 0; //account data
 
-int main(void)
-{
-	printf("second  commit");
-	return 0;
+void make(){
+	printf("Your ID: ");
+	scanf("%s",ID_pass[0]);
+	printf("\nYour Passwowrd: ");
+	scanf("%s",ID_pass[1]);
 }
+
+void withdraw(){
+	printf("Your account had been withdrawn");
+	for(int k = 0; k<2 ; k++){
+		for(int i = 0; i<9; i++){
+			ID_pass[k][i] = NULL;
+		}
+	}
+}
+
+void menu();
+
+int check(char id_passwd[]){  //ID and Password checking func
+	if(((int)'a'<
